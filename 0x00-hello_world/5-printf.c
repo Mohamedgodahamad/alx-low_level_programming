9c8b46cf54549f7cad4d6c98b4ab7ad982932197
Mohamedgodahamad <mohamedgoda123200@gmail.com>
2023-08-10 20:16:59 -0700

    allah

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+# include <stdio.h>
+/**
+ * main -Entry point
+ *
+ * Description: A C program that prints with printf
+ *
+ * Return: always return 0 (succes)
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
