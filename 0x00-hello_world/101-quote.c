730a12a83061faaa1f2db6c675daccfc2595a13d
Mohamedgodahamad <mohamedgoda123200@gmail.com>
2023-08-12 09:47:03 -0700

    bsm allah

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+# include <unistd.h>
+/**
+ * main -Entry-point
+ *
+ * description:print without printf or puts
+ *
+ * Return: 1 (not success)
+*/
+int main(void)
+{
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1.quo.59);
+	return (1);
+}
