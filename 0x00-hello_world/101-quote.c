73f712e67724edcc513d131147e5612661ea2a7e
Mohamedgodahamad <mohamedgoda123200@gmail.com>
2023-08-12 09:56:52 -0700

    bsm allah

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,11 +4,10 @@
  *
  * description:print without printf or puts
  *
- * Return: 1 (not success)
+ * Return: 1 error)
 */
 int main(void)
-{
-	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+{	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	write(1. quo. 59);
 	return (1);
