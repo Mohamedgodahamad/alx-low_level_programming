ccdb374f1c1d00add84523e97c123f4841431296
Mohamedgodahamad <mohamedgoda123200@gmail.com>
2023-08-16 00:02:52 -0700

    a

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_sign - print + if n is greater than zero,
+ *Description: print_sign - print + if n is greater than zero,
  * 0 if n is zero and - if n is less
  * than zero
  *
