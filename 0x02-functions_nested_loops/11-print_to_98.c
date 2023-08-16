a186c21f60173c232481567e34f6e12da7227172
Mohamedgodahamad <mohamedgoda123200@gmail.com>
2023-08-16 07:22:50 -0700

    print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -16,7 +16,7 @@ void print_to_98(int n)
 			if (i != 98)
 				printf("%d, ", i);
 			else if (i == 98)
-				printf("%d, ", i);
+				printf("%d\n", i);
 		}
 	}
 	else if (n >= 98)
@@ -26,7 +26,7 @@ void print_to_98(int n)
 			if (j != 98)
 				printf("%d, ", j);
 			else if (j == 98)
-				printf("%d, ", j);
+				printf("%d\n", j);
 		}
 	}
 }
