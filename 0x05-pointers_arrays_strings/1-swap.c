71a6a90fab849defe1a310ba099791727c21419c
Mohamedgodahamad <mohamedgoda123200@gmail.com>
2023-08-21 10:42:16 -0700

    the ticknique of swaping to numbers to take each of them the value of the other

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * swap_int - swaps the values of two integers
+ * @a: integer to swap
+ * @b: integer to swap
+ */
+void swap_int(int *a, int *b)
+{
+	int m;
+	int k;
+
+	k = *b;	
+	m = *a;
+	*b = m;
+	*a = k;
+}
