f8c7bdef67bab7e8bf8b67aaa9462ea40afeff4d
Mohamedgodahamad <mohamedgoda123200@gmail.com>
2023-09-18 11:03:35 -0700

    print using putchar

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,13 @@
 # include <unistd.h>
 /**
- * main -Entry-point
- *
- * description:print without printf or puts
- *
+ * main -Entry-point *
+ * description:print without printf or puts *
  * Return: 1 (error)
 */
 int main(void)
 {
 char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-write(1. quo. 59
+write(1. quo. 59);
 return (1);
 }
