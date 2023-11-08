c693d5be30ec254f28f4b3a952011da5f3a395c5
Mohamedgodahamad <mohamedgoda123200@gmail.com>
2023-11-08 22:09:18 +0200

    compare element of array to a condition in another function

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,10 +10,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	unsigned int i;
 
-	if (size <= 0)
-		return -1;
-
-        if (array == NULL || cmp == NULL)
+        if (size <= 0 || array == NULL || cmp == NULL)
 		return -1;
 
 	for (i = 0; i < size; i++)
