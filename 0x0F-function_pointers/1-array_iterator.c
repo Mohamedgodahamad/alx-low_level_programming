7b122a64aba21770f9cebc82d76eb66fc1688501
Mohamedgodahamad <mohamedgoda123200@gmail.com>
2023-11-08 21:14:05 +0200

    print elements of array

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,11 +11,11 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
 
-	if(action == NULL || array == NULL)
+	if (action == NULL || array == NULL)
 		return;
 
 	for (i = 0; i < size; i++)
 	{
-		action(&array[i]);
+		action(array[i]);
 	}
 }
