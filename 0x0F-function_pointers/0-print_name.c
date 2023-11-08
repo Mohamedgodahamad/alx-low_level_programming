829b9d30a60b644139680fb97e920caf9b07f01c
Mohamedgodahamad <mohamedgoda123200@gmail.com>
2023-11-08 19:54:06 +0200

    print name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -6,10 +6,11 @@
  * @name: string to add
  * @f:pointer to function
  * Return: nothing
-*/
+ */
 void print_name(char *name, void (*f)(char *))
 {
         if (name == NULL || f == NULL)
 		return;
+
 	f(name);
 }
